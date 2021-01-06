77ff91ae19f27a1864fda92acfd71f1376f3c0ff
Alvatador <69437783+Alvatador@users.noreply.github.com>
2021-01-06 15:29:37 +0300

    Update hesapmakinesi.sh

--- hesapmakinesi.sh
+++ hesapmakinesi.sh
@@ -16,18 +16,20 @@
 
 
     echo ""
-    echo -e "$fiuscha [1]*Toplama                  [2]*Çıkarma"
-    echo -e "$cafe [3]*Çarpma                   [4]*Bölme"
-    echo -e "$red"
+    echo -e " $fiuscha [1] ✔ Toplama                  [2] ✔ Çıkarma"
+    echo -e " $cafe [3] ✔ Çarpma                   [4] ✔ Bölme"
+    echo -e " $cyan [5] ✔ Çevre Hesaplama          [6] ✔ Alan Hesaplama" 
+    echo -e " $red"
 
     echo -n "İşlemin Yanındaki Sayıyı Yazınız!: "
     read n
 
     if [[ $n = "1" ]] ; then
         echo -e "Toplama İşlemi Seçildi!"
-        echo "Bir Sayı Giriniz"
+        echo -e "$cyan"
+        echo "Bir Sayı Giriniz: "
         read x
-        echo "2. Bir Sayı Giriniz"
+        echo "2. Bir Sayı Giriniz: "
         sleep 2
         clear
         read y 
@@ -40,9 +42,10 @@
 
     if [[ $n = "2" ]] ; then
         echo -e "Çıkarma İşlemi Seçildi!"
-        echo "Bir Sayı Giriniz"
+        echo -e "$cyan"
+        echo "Bir Sayı Giriniz: "
         read x
-        echo "2. Bir Sayı Giriniz"
+        echo "2. Bir Sayı Giriniz: "
         sleep 2
         clear
         read y 
@@ -55,9 +58,10 @@
 
     if [[ $n = "3" ]] ; then
         echo -e "Çarpma İşlemi Seçildi!"
-        echo "Bir Sayı Giriniz"
+        echo -e "$cyan"
+        echo "Bir Sayı Giriniz: "
         read x
-        echo "2. Bir Sayı Giriniz"
+        echo "2. Bir Sayı Giriniz: "
         sleep 2
         clear
         read y 
@@ -70,9 +74,10 @@
 
     if [[ $n = "4" ]] ; then
         echo -e "Bölme İşlemi Seçildi!"
-        echo "Bir Sayı Giriniz"
+        echo -e "$cyan"
+        echo "Bir Sayı Giriniz: "
         read x
-        echo "2. Bir Sayı Giriniz"
+        echo "2. Bir Sayı Giriniz: "
         sleep 2
         clear
         read y 
@@ -82,3 +87,105 @@
     clear
     bash hesapmakinesi.sh
     fi
+
+    if [[ $n = "5" ]] ; then
+        echo -e "Çevre Hesaplama İşlemi Seçildi!"
+        echo -e "$cyan"
+        echo -e "[1] ✔ Kare          [2] ✔ Dikdörtgen"
+        echo -e "[3] ✔ Üçgen"
+        echo -e "$red"
+
+        echo -n "İşlemi Seçiniz: "
+        read n
+      if [[ $n = "1" ]] ; then 
+      echo -e "Kare Çevresini Hesapla Seçildi! "
+      echo -e "$cyan"
+      echo -e "Karenin Bir Kenar Uzunluğunu Yazınız!" 
+      read x
+      clear
+      echo "Cevap="$(($x * 4)) 
+      sleep 5
+      clear
+      bash hesapmakinesi.sh
+      fi
+
+     if [[ $n = "2" ]] ; then 
+      echo -e "Dikdörtgen Çevresini Hesapla Seçildi! "
+      echo -e "$cyan"
+      echo -e "Dikdörtgenin Uzun Kenarını Yazınız! " 
+      read x
+      echo -e "Dikdörtgenin Kısa Kenarını Yazınız! "
+      read y 
+      clear
+      echo "Cevap="$(( ($x + $y) * 2 ))
+      sleep 5
+      clear
+      bash hesapmakinesi.sh
+      fi
+      if [[ $n = "3" ]] ; then 
+
+      sleep 5
+      clear
+      echo -e "Üçgen Çevresini Hesapla Seçildi! "
+      echo -e "$cyan"
+      echo -e "Üçgenin Bir Kenarını Kenarını Yazınız! " 
+      read x
+      echo -e "Üçgenin 2. Kenarını Yazınız! "
+      read y 
+      echo -e "Üçgenin 3. Kenarını Yazınız! "
+      read z
+      clear
+      echo "Cevap="$(( ($x + $y) + $z ))
+      sleep 5
+      clear
+      bash hesapmakinesi.sh
+      fi  
+fi      
+
+    if [[ $n = "6" ]] ; then
+        echo -e "Alan Hesaplama İşlemi Seçildi!"
+        echo -e "$cyan"
+        echo -e "[1] ✔ Kare          [2] ✔ Dikdörtgen"
+        echo -e "[3] ✔ Üçgen"
+        echo -e "$red"
+        echo -n "İşlemi Seçiniz "
+        read n
+    if [[ $n = "1" ]] ; then
+        echo -e "Kare Alan Hesaplama İşlemi Seçildi"
+        echo -e "$cyan"
+        echo -e "Karenin 1 Kenarını Yazınız: "
+        read x 
+        echo "Cevap="$(($x * $x))
+        sleep 5 
+        clear 
+        bash hesapmakinesi.sh
+     fi   
+
+    if [[ $n = "2" ]] ; then
+        echo -e "Dikdörtgen Alan Hesaplama İşlemi Seçildi"
+        echo -e "$cyan"
+        echo -e "Dikdörtgenin Kısa Kenarını Yazınız: "
+        read x 
+        echo -e "Dikdörtgenin Uzun Kenarını Yazınız: "
+        read y 
+        echo "Cevap="$(($x * $y))
+        sleep 5 
+        clear 
+        bash hesapmakinesi.sh
+     fi  
+
+     if [[ $n = "3" ]] ; then
+        echo -e "Üçgen Alan Hesaplama İşlemi Seçildi"
+        echo -e "$cyan"
+        echo -e "Üçgenin 1. Kenarını Yazınız: "
+        read x 
+        echo -e "Üçgenin 2. Kenarını Yazınız: "
+        read y 
+        echo -e "Üçgenin 3. Kenarını Yazınız: "
+        read z
+        echo "Cevap="$(( ($x + $y) + $z ))
+        sleep 5 
+        clear 
+        bash hesapmakinesi.sh
+     fi   
+fi        
